43850917b36b910a8bb1a4f889ec19d1ca02cd15
Vladislav <vl-borod@yandex.ru>
2024-10-05 02:36:07 +0300

    aaa

--- script.js
+++ script.js
@@ -1,42 +1,70 @@
 // Долгосрочныq токен
-const link = 'http://localhost:8010/proxy/api/v4/leads';
+const link = 'http://localhost:8010/proxy/api/v4/leads?limit=3';
 const access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImZjYTJjMDViZmE1MDVkYzhhZGIzODQxYzkxOTA0YzNmYzZhOTczN2ZmNTM1MzA2ZDlmMDgzNmNjMWY3YjNkNGJmMTQwOWQwNWRlODVmODQ1In0.eyJhdWQiOiIwODk0MWE2ZS1iMGVkLTRhYWQtOWU2OS0yYmRhNTQ4YTcwZWMiLCJqdGkiOiJmY2EyYzA1YmZhNTA1ZGM4YWRiMzg0MWM5MTkwNGMzZmM2YTk3MzdmZjUzNTMwNmQ5ZjA4MzZjYzFmN2IzZDRiZjE0MDlkMDVkZTg1Zjg0NSIsImlhdCI6MTcyODA2MTM0NiwibmJmIjoxNzI4MDYxMzQ2LCJleHAiOjE3MzA4NTEyMDAsInN1YiI6IjExNTgxODQyIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxOTc5MzQ2LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiNDgwNTk2M2QtMWQ1Ny00MWRmLThjNDQtMDlkZTIwZDI4ZDZjIiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.NRd3efey5ZaxC6B3T12VMQTqNdzhnGOkg5HzeECBZiiGarDrUa7GAewiw-qMkGTqQqPAYFobwsP-NiTkPWWUON-Hn9VIMu2t1nFDL27w903DAI8IgL4B8LjnriwaFLvee2anR28SLYIkYDatKuRM7XPyNv4HNtohvxK_9H_Ldh7JwNjxGRdCQfnYeMUzdPenJiY7JEuK8mP8PreBLXXpUaAUIRoxI5VaRCssAbpBnBRQUFC0UO6IxlHRAZbbAw9wit1et6XSKysR_NQJJHWUkqbKFts00EJRYH8EQjpzsgqLdgMrCSPU1BHwX5-S99zyPmjJwWuFmhSGMqalbB8P7A';
 const headers = {
-    'Authorization': `Bearer ${access_token}`,
-    'content-type': 'application/json',
+  'Authorization': `Bearer ${access_token}`,
+  'content-type': 'application/json',
 };
 
-fetch(link, {
+let currentPage = 1;
+
+let timeInterval = null;
+
+const baseUrl = 'http://localhost:8010/proxy/api/v4/leads';
+
+
+function fetchWithDynamicParams(baseUrl, paramsObj) {
+  
+  const url = new URL(baseUrl);
+  const params = new URLSearchParams(paramsObj);
+  console.log(params)
+  fetch(`${url}?${params}`, {
     method: 'GET',
     headers: headers,
     redirect: 'follow',
-})
-.then(response => {
+  })
+  .then(response => {
     const code = response.status;
     if (code < 200 || code > 204) {
-        const errors = {
-            400: 'Bad request',
-            401: 'Unauthorized',
-            403: 'Forbidden',
-            404: 'Not found',
-            500: 'Internal server error',
-            502: 'Bad gateway',
-            503: 'Service unavailable',
-        };
-        throw new Error(errors[code] || 'Undefined error', { cause: code });
+      const errors = {
+        400: 'Bad request',
+        401: 'Unauthorized',
+        403: 'Forbidden',
+        404: 'Not found',
+        500: 'Internal server error',
+        502: 'Bad gateway',
+        503: 'Service unavailable',
+      };
+      throw new Error(errors[code] || 'Undefined error', { cause: code });
     }
-    return response.json(); 
-})
-.then(data => {
-    console.log(data._embedded.leads); 
+    return response.json();
+  })
+  .then(data => {
+    console.log(data._embedded.leads);
     let rows = '';
     data._embedded.leads.forEach(lead => {
       rows += `<tr><td>${lead.name}</td><td>${lead.price}</td><td>${lead.id}</td></tr>`
+    })
+    document.getElementById('tableRows').innerHTML = rows;
+    console.log(data._embedded.leads.length)
+    if ( data._embedded.leads.length > 0){
+      currentPage = currentPage + 1
+    } else {
+      clearInterval(timeInterval);
+    }
   })
-  document.getElementById('tableRows').innerHTML = rows;
-})
-.catch(error => {
+  .catch(error => {
     console.error('Ошибка:', error.message);
     console.error('Код ошибки:', error.cause);
-});
+    clearInterval(timeInterval);
+  });
+}
 
+const dynamicParams = {
+  page: currentPage,
+  limit: 3
+};
+
+timeInterval = setInterval( function() {
+  fetchWithDynamicParams(baseUrl, dynamicParams)
+}, 1000);
