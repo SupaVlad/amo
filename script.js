e879658450e9de544972b3ea97524ab1e597f32c
Vladislav <vl-borod@yandex.ru>
2024-10-06 00:29:09 +0300

    haha end

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-// Долгосрочный токен
 const access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImZjYTJjMDViZmE1MDVkYzhhZGIzODQxYzkxOTA0YzNmYzZhOTczN2ZmNTM1MzA2ZDlmMDgzNmNjMWY3YjNkNGJmMTQwOWQwNWRlODVmODQ1In0.eyJhdWQiOiIwODk0MWE2ZS1iMGVkLTRhYWQtOWU2OS0yYmRhNTQ4YTcwZWMiLCJqdGkiOiJmY2EyYzA1YmZhNTA1ZGM4YWRiMzg0MWM5MTkwNGMzZmM2YTk3MzdmZjUzNTMwNmQ5ZjA4MzZjYzFmN2IzZDRiZjE0MDlkMDVkZTg1Zjg0NSIsImlhdCI6MTcyODA2MTM0NiwibmJmIjoxNzI4MDYxMzQ2LCJleHAiOjE3MzA4NTEyMDAsInN1YiI6IjExNTgxODQyIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxOTc5MzQ2LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiNDgwNTk2M2QtMWQ1Ny00MWRmLThjNDQtMDlkZTIwZDI4ZDZjIiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.NRd3efey5ZaxC6B3T12VMQTqNdzhnGOkg5HzeECBZiiGarDrUa7GAewiw-qMkGTqQqPAYFobwsP-NiTkPWWUON-Hn9VIMu2t1nFDL27w903DAI8IgL4B8LjnriwaFLvee2anR28SLYIkYDatKuRM7XPyNv4HNtohvxK_9H_Ldh7JwNjxGRdCQfnYeMUzdPenJiY7JEuK8mP8PreBLXXpUaAUIRoxI5VaRCssAbpBnBRQUFC0UO6IxlHRAZbbAw9wit1et6XSKysR_NQJJHWUkqbKFts00EJRYH8EQjpzsgqLdgMrCSPU1BHwX5-S99zyPmjJwWuFmhSGMqalbB8P7A';
 const headers = {
   'Authorization': `Bearer ${access_token}`,
   'content-type': 'application/json',
 };
 let preloader;
-let currentPage = [1, 1]; // Хранит номера страниц для двух ссылок
+let currentPage = [1, 1];
 
 const baseUrls = [
-  'http://localhost:8010/proxy/api/v4/leads', // первая ссылка
-  'http://localhost:8010/proxy/api/v4/tasks' // вторая ссылка
+  'http://localhost:8010/proxy/api/v4/leads', 
+  'http://localhost:8010/proxy/api/v4/tasks' 
 ];
 
-let rows = ''; // Для хранения объединенных данных
+let rows = ''; 
 let allLeads = [];
 let allTasks = [];
 
-// Флаги для отслеживания окончания данных по лидам и задачам
 let hasMoreLeads = true;
 let hasMoreTasks = true;
 
-// Добавьте элемент предзагрузчика в HTML
 document.addEventListener("DOMContentLoaded", function() {
   preloader = document.getElementById('preloader');
-  console.log(preloader); // Это должно записать элемент предзагрузчика в консоль
-  // Остальной ваш JavaScript-код здесь
+  console.log(preloader);
 });
 
 function fetchNextPage() {
@@ -34,15 +30,12 @@ function fetchNextPage() {
     fetchTasks();
   } else {
     clearInterval(timeInterval);
-    renderData(); // Render data after receiving all data
-
+    renderData();
   }
-  
 }
 
 const timeInterval = setInterval(fetchNextPage, 1000);
 
-// Функция для получения данных по лидерам
 function fetchLeads() {
   const url = new URL(baseUrls[0]);
   const params = new URLSearchParams({ page: currentPage[0], limit: 3 });
@@ -54,18 +47,17 @@ function fetchLeads() {
   })
     .then(response => response.json())
     .then(data => {
-      allLeads = [...allLeads, ...data._embedded.leads]; // Сохраняем лидов
+      allLeads = [...allLeads, ...data._embedded.leads];
       if (data._links && data._links.next) {
-        currentPage[0]++; // Увеличиваем страницу для лидов, если есть следующая страница
+        currentPage[0]++;
       } else {
-        hasMoreLeads = false; // Устанавливаем флаг, что данных больше нет
+        hasMoreLeads = false;
       }
-      if (hasMoreTasks) fetchTasks(); // Вызвать fetchTasks только после завершения fetchLeads
+      if (hasMoreTasks) fetchTasks();
     })
     .catch(error => console.error('Ошибка получения лидов:', error));
 }
 
-// Функция для получения данных по задачам
 function fetchTasks() {
   const url = new URL(baseUrls[1]);
   const params = new URLSearchParams({ page: currentPage[1], limit: 3 });
@@ -77,24 +69,32 @@ function fetchTasks() {
   })
     .then(response => response.json())
     .then(data => {
-      allTasks = [...allTasks, ...data._embedded.tasks]; // Конкатенируем новые задачи к существующему массиву
+      allTasks = [...allTasks, ...data._embedded.tasks];
       if (data._links && data._links.next) {
-        currentPage[1]++; // Увеличиваем страницу для задач, если есть следующая страница
+        currentPage[1]++;
       } else {
-        hasMoreTasks = false; // Устанавливаем флаг, что данных больше нет
+        hasMoreTasks = false;
       }
     })
     .catch(error => console.error('Ошибка получения задач:', error));
 }
+
 let rowsArray = [];
-// Функция для сопоставления данных и рендеринга
 function renderData() {
-  let newRows = ''; // новая переменная для хранения новых данных
+  let newRows = ''; 
   rowsArray = [];
   allLeads.forEach((lead, index) => {
-    // Находим задачи, связанные с этим лидом
     const task = allTasks[index];
     const tasksForLead = allTasks.filter(task => task.lead_id === lead.id);
+    function pad(number) {
+      return (number < 10 ? '0' : '') + number;
+    }
+
+    const createdAtDate = new Date(task.complete_till * 1000);
+    const formattedDate = `${pad(createdAtDate.getDate())}.${pad(createdAtDate.getMonth() + 1)}.${createdAtDate.getFullYear()}`;
+    const currentDate = new Date();
+
+    
     rowsArray.push(`
       <div class="accordion">
         <button class="menu-button">
@@ -104,7 +104,19 @@ function renderData() {
           <span class="icon">&plus;</span>
         </button>
         <div class="content">
-        ${task ? `<p>ID Задачи: ${task.id}</p>` : '<p>Нет задач</p>'}
+        ${task ? `<p>ID Задачи: ${task.id}</p><p>Текст задачи: ${task.text}</p><p>Сделать до: ${formattedDate} </p><p>Статус ближайшей задачи: 
+          ${(() => {
+          const currentDate = new Date();
+          if (createdAtDate.getFullYear() === currentDate.getFullYear() && createdAtDate.getMonth() === currentDate.getMonth()) {
+            if (createdAtDate.getDate() === currentDate.getDate()) {
+              return '<svg width="20px" height="20px" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet"><circle fill="#78B159" cx="18" cy="18" r="18"></circle></svg>';
+            } else if (createdAtDate.getDate() === currentDate.getDate() - 1) {
+              return '<svg width="20px" height="20px" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet"><circle fill="#DD2E44" cx="18" cy="18" r="18"></circle></svg>';
+            }
+          }
+          return '<svg width="20px" height="20px" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet"><circle fill="#FDCB58" cx="18" cy="18" r="18"></circle></svg>';
+        })()}
+        </p>` : '<p>Нет задач</p>'}
         </div>
       </div>
       `);
@@ -113,14 +125,13 @@ function renderData() {
   rows = rowsArray.join('');
   document.getElementById('tableRows').innerHTML = rows;
 
-  // Добавляем функционал для аккордеонов
   const menuBtns = document.querySelectorAll(".menu-button");
   menuBtns.forEach(menuBtn => {
     menuBtn.addEventListener("click", function () {
       const activeAccordion = document.querySelector(".menu-button.open");
       if (activeAccordion && activeAccordion !== this) {
         activeAccordion.nextElementSibling.style.display = 'none';
-        activeAccordion.nextElementSibling.style.height = '0'; // Set height to 0 when closed
+        activeAccordion.nextElementSibling.style.height = '0';
         activeAccordion.classList.remove("open");
       }
 
@@ -136,9 +147,8 @@ function renderData() {
     });
   });
 
-  // Останавливаем обновление, если больше нет данных для обеих ссылок
   if (!hasMoreLeads && !hasMoreTasks) {
-    clearInterval(timeInterval); // Остановка периодических запросов
+    clearInterval(timeInterval); 
   }
-  preloader.style.display = 'none'; // Скрываем предзагрузчик после отрисовки данных
+  preloader.style.display = 'none';
 }
